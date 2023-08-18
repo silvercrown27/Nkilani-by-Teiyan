0235e8f7d6556e076ef91de307e19722c303370e
Bradley <bradabwao14@gmail.com>
2023-08-18 12:28:16 +0300

    landing page

---
+++ onlineshop/main/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def landing_page(requests):
+    return render(requests, "index.html")
+
+
+def cart_page(requests):
+    return render(requests, "cart.html")
+
+
+def contact_page(requests):
+    return render(requests, " contact.html")
+
+
+def checkout_page(requests):
+    return render(requests, "checkout.html")
+
+
+def detail_page(requests):
+    return render(requests, "detail.html")
+
+
+def shop_page(requests):
+    return render(requests, "shop.html")
+
+
+def signin_page(requests):
+    return render(requests, "Sign in.html")
+
+
+def signup_page(requests):
+    return render(requests, "Sign Up.html")
+
+
+def wishlist_page(requests):
+    return render(requests, "wishlist.html")
